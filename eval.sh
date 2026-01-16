8a4c700c40b242672ccd2a6d922728e5a82a4d9d
Yiwei He <oldgirafferiver@gmail.com>
2026-01-16 11:02:44 +0000

    update

--- eval.sh
+++ eval.sh
@@ -2,7 +2,7 @@ python classification_experiment_multimethod.py \
     --calibration-dir /mnt/nas_d/data/deepfake/collected_dataset/ForenSynths/test/cyclegan/winter/0_real \
     --test-dir /mnt/nas_d/data/deepfake/collected_dataset/ForenSynths/test \
     --criterion score_laplacian_fast \
-    --num-noise 16 \
+    --num-noise 64 \
     --batch-size 2 \
     --output-dir results
 
